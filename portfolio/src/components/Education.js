c285b975a03fdff5da6958cef76f32024596bf2f
courtney long <courtneylong877@gmail.com>
2022-01-25 20:28:22 -0500

    lets see now

--- portfolio/src/components/Education.js
+++ portfolio/src/components/Education.js
@@ -30,7 +30,7 @@ function Education() {
                   </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                  <h5
+                  <h5 className= "fixThePadding"
                   style = {{
                       textAlign: "center",
                       paddingBottom: "50px",
@@ -39,7 +39,7 @@ function Education() {
                   <p
                   style = {{
                     textAlign: "center",
-                    paddingBottom: "50px"
+                    paddingBottom: "10px"
                 }}>
                     After graduating highschool in 2015, I attended collage at Kennesaw State University.  I initially had registered my major as undecided; however, after receiving minimal advice from an advisor, I chose Mechanical Engineering.  I graduated with a Bachelor of Science in Mechanical Engineering in May of 2021.  Before even receiving my degree, I began working full-time at a company I had been in a co-op program with for two years.  At this point, I knew that the degree was not the one for me, but I worked hard at my job for a year before deciding I wanted to pursue a career in something I would actually enjoy.  I began working part-time at my company and signed up for a Full-Stack Website Development course hosted through Georgia Tech University.  It was a full-time program lasting for three months.  I realized then that I found what I love to do, and am now working on pursuing certifications and creating any application that I can create.
                   </p>
